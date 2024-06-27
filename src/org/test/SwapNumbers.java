17417602e43a53404edba3bfcdfd322c1ec1e450
annieivya10 <anniedivya01@gmail.com>
2024-06-27 22:28:45 +0530

    Few lines of code added

---
+++ src/org/test/SwapNumbers.java
@@ -0,0 +1,22 @@
+package org.test;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=7,c;
+		
+		System.out.println("Values before swapping:a="+ a);
+		System.out.println("Values before swapping:b="+ b);
+		
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("Values after swapping:a="+ a);
+		System.out.println("Values after swapping:b="+ b);
+
+		
+	}
+	
+}
